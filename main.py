94403e6ddcf1357df73601964ebc3f0becfdc0fe
sasimohan89 <sm9474699@gmail.com>
2020-01-15 20:29:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# fundamental data types # int and float
+# name = input('Name? ')
+# print(type(2 / 4))
+# print(type(2 + 4))
+# print(type(-5))
+# print(5 % 3)  # remainder
+# print(5 ** 4) # to the power of
+# print(15 // 4)  # quotient
+
+# math functions
+print(round(3.1))
+
