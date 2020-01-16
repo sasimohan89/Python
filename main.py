08306d89e17ac13ed6ade09193a9741d4cff9772
sasimohan89 <sm9474699@gmail.com>
2020-01-16 20:43:41 +0000

    built in functions

--- main.py
+++ main.py
@@ -8,8 +8,55 @@
 # print(15 // 4)  # quotient
 
 # math functions
-print(round(3.1))
+# print(round(3.1))
 
-#constants
-PI = 3.14
+# #constants
+# PI = 3.14
+# a,b,c = 1,2,3
 
+# # augmented assignment operators
+# x = 1
+# x += 3
+# print(x)
+
+# # strings
+# sample = 'xasdfg'
+# print(sample)
+# print(type(sample))
+# long_string = '''
+# abc
+# def
+# ghi
+# '''
+# print(long_string)
+
+# string concatenation
+# print('hello' + ' ' + 'sasi')
+
+# # type conversion
+# str(100)
+# int(str(100))
+
+# # Escape sequence
+# print("\t It\'s \"kind of\" sunny today \n Hah!")
+
+# # Formatted string
+# name = 'Andrew'
+# age = 33
+# print(f'hi {name}. You are {age} years old')  # age was converted to string automatically
+
+# # string memory & slicing
+# shell = 'abcdefghij'
+# print(shell[1])
+# print(shell[0:5:2]) # start:stop:stepover
+# print(shell[::-1])  # shit, reversing a string is that easy!
+
+# Built in funcitons
+text = 'to be or not to be'
+print(len(text))
+print(text[0:len(text)-2])
+print(text.upper())
+print(text.capitalize())
+print(text.find('or'))
+print(text.replace('or','and'))
+print(text)  # Strings are IMMUTABLE
