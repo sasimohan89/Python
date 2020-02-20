3ae2563486476d18811133663faac0b44378b2a5
sasimohan89 <sm9474699@gmail.com>
2020-02-20 17:28:19 -0500

    Dunder Methods

--- Advanced Python OOP/main.py
+++ Advanced Python OOP/main.py
@@ -1,31 +1,12 @@
-class User():
-    def __init__(self, email):
-        self.email = email
+class SuperList(list):
+    def __len__(self):
+        return 1000
 
-    def sign_in(self):
-        print('Logged in')
 
-    def attack(self):
-        print('Do nothing')
+super_list1 = SuperList()
 
-class Wizard(User):
-    def __init__(self, name, power, email):
-        super().__init__(email) # takes from class outside
-        self.name = name
-        self.power = power
-
-    def attack(self):
-        User.attack(self)
-        print(f'Attacking with a power of {self.power}')
-
-class Archer(User):
-    def __init__(self, name, num_arrows):
-        self.name = name
-        self.num_arrows = num_arrows
-
-    def attack(self):
-        print(f'Left with arrows: {self.num_arrows}')
-
-wizard1 = Wizard('Tanker', 100, 'a@a.com')
-print(wizard1.email)
-
+print(len(super_list1))
+super_list1.append(5)
+print(super_list1[0])
+print(issubclass(SuperList, list))
+print(issubclass(list, object))
